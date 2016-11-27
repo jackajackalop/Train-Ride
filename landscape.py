c4288f1081368a37c5b9d5893cd7375914ed8baf
Yixin He <Yixin He>
2016-11-27 01:23:34 -0500

    basic user input added

--- landscape.py
+++ landscape.py
@@ -5,17 +5,23 @@ class land(object):
 	def __init__(self,width,height,temp,weather):
 		self.width =width
 		self.height = height
-		self.scrollX = 0
 		self.temp = temp
 		self.weather = weather
-		self.grass=	(110,140,30)
+		self.grass=	(120,130,40)
 		self.sky = (190,210,200)
 		self.timeDelay = 0
+		self.speed = 0
 
 	def temperature(self):
 		self.hueShift = (0,0,0)
 
-	def atmosphericPersp(self,color,distance): 
+	def changeSpeed(self,x):
+		if(abs(self.speed+x)<=5):
+			self.speed+=x
+
+
+	@staticmethod
+	def atmosphericPersp(color,distance): 
 	#things in distance are lighter and less saturated
 		newColor = list(color)
 		for color in range(3):
@@ -31,15 +37,17 @@ class land(object):
 		pygame.draw.rect(surf,self.sky,(0,0,w,h*3/4))
 		pygame.draw.rect(surf,self.grass,(0,h*3/4,w,h/4))
 
-	def shift(self,pts,space,time):
+	def shift(self,pts,speed):
 		for index in range(0,len(pts)-1):
-			pts[index][0]-=space/time
+			pts[index][0]-=speed
 		return pts
 
 	def animate(self,surf,pts,color,space,time):
 		self.timeDelay+=1
 		w,h = self.width,self.height
-		pts = self.shift(pts,space,time)
+		speedIncr = (space/time)/6
+		speed = space/time+self.speed*speedIncr
+		pts = self.shift(pts,speed)
 		pygame.draw.polygon(surf,color,pts)
 		return pts
 		
@@ -48,19 +56,18 @@ class plain(land):
 	def __init__(self,width,height,temp,weather):
 		super().__init__(width,height,temp,weather)
 		self.far = []
-		self.farTreesColor = self.atmosphericPersp(self.grass,3)
+		self.farTreesColor = super().atmosphericPersp(self.grass,3)
 		self.farPts=50
 		self.farSpace = self.width/self.farPts
-		self.farTreesPoints(0,self.farPts+2)
+		self.farTreesPts(0,self.farPts+2)
 		self.farTime = 30
 		self.mid = []
-
-		
-	def farTreesPoints(self,start,times):
+	#TODO draw more ahead of train
+	def farTreesPts(self,start,times):
 		w,h = self.width,self.height
 		for bump in range(times):
 			self.far.append([start+self.farSpace*bump,
-							random.randint(h*3/4-h/30,h*3/4-h/50)])
+							random.randint(h*3//4-h//30,h*3//4-h//50)])
 		self.far.append([w+1*self.farSpace,h*3/4])
 		self.far.append([0,h*3/4])
 
@@ -70,7 +77,7 @@ class plain(land):
 		if(self.timeDelay%self.farTime==0):
 			self.far.pop(0)
 			del self.far[-2:]
-			self.farTreesPoints(self.width+self.farSpace,1)
+			self.farTreesPts(self.width+self.farSpace,1)
 
 class river(land):
 	def __init__(self,width,height,temp,weather,distance):
@@ -78,11 +85,11 @@ class river(land):
 		self.distance = distance
 		self.river= []
 		self.riverPts = 6
-		self.riverSpace = self.width/self.riverPts
+		self.riverSpace = self.width/(self.riverPts)
 		self.riverTime = 10*(distance+1)
 		self.riverColor = (100,100,150)
 		self.riverPoints(0,self.riverPts+2)
-
+	#TODO draw more ahead
 	def riverPoints(self,start,times):
 		w,h = self.width,self.height
 		h1 =h-h*self.distance//12
@@ -107,19 +114,77 @@ class river(land):
 			self.river.pop(-2)
 			self.riverPoints(self.width+self.riverSpace,1)
 			
-class hills(land):
-	def __init__(self,width,height,temp,weather):
-		super().__init__(width,height,temp,weather)
-		self.hills=[]
-
-	def hillPoints(self,start,times):
-		
-
 class mountains(land):
+	def __init__(self,width,height,temp,weather,size):
+		super().__init__(width,height,temp,weather)
+		self.size = size
+		self.mountainsB=[]
+		self.mountainsF = []
+		self.mtNumber = random.randint(3,5)
+		self.BNumber = self.mtNumber-2
+		self.mountainSpaceF = self.width//self.mtNumber
+		self.mountainSpaceB = self.width//(self.mtNumber-2)
+		self.mountainTime = 800
+		self.BSplits = []
+		self.FSplits = []
+		self.mountainsB,self.BSplits=self.mountainPts(self.mountainsB,
+			self.BSplits,self.BNumber,self.mountainSpaceB,0,self.BNumber+2)
+		self.mountainsF,self.FSplits = self.mountainPts(self.mountainsF,
+			self.FSplits,self.mtNumber,self.mountainSpaceF,0,self.mtNumber+2)
+		self.color = (100,130,140)
+		self.farColor = super().atmosphericPersp(self.color,2)
+
+	def mountainPts(self,pts,splits,number,space,start,times):
+		widthMargin = self.width//(2*number)
+		y = (self.height*3/4)//(self.size+.5)
+		heightMargin = ((self.height*3/4)-y)//5
+		previousHeight = y
+		for mountains in range(times):
+			h = int(start+space*mountains)
+			h1 = random.randint(h,h+widthMargin)
+			h2= h+space
+			pts.append([h1,self.height*3/4])
+			mountainBumps = random.randint(8,12)
+			splits.append(mountainBumps+1)
+			ptDist = (h2-h1)//mountainBumps
+			ptMargin = ptDist//2
+			for bumps in range(mountainBumps):
+				ptX = random.randint(h1+ptDist*bumps,h1+ptDist*bumps+ptMargin)
+				if(previousHeight+heightMargin)>=(self.height*3/4):
+					previousHeight=self.height*3/4 - heightMargin 
+				ptY = random.randint(previousHeight-heightMargin,
+									previousHeight+heightMargin)
+				previousHeight = ptY
+				pts.append([ptX,ptY])
+		pts.append([h2,self.height*3/4])
+		pts.append([0,self.height*3/4])
+		return pts,splits
+	#TODO removes mountain based on h1, but should be based on h2
+	def draw(self,surf):
+		self.mountainsB=super().animate(surf,self.mountainsB,
+			self.farColor,self.mountainSpaceB,self.mountainTime*3)
+		self.timeDelay-=1
+		self.mountainsF=super().animate(surf,self.mountainsF,
+			self.color,self.mountainSpaceF,self.mountainTime)
+		if(self.timeDelay%(self.mountainTime*3)==0):
+			del self.mountainsB[:self.BSplits[0]]
+			self.BSplits.pop(0)
+			del self.mountainsB[-2:]
+			self.mountainPts(self.mountainsB,self.BSplits,self.BNumber,
+						self.mountainSpaceB,self.width+self.mountainSpaceB,1)
+		if(self.timeDelay%self.mountainTime==0):
+			self.FSplits.pop(0)
+			del self.mountainsF[-2:]
+			del self.mountainsF[:self.FSplits[0]]
+			self.mountainPts(self.mountainsF,self.FSplits,self.mtNumber,
+						self.mountainSpaceF,self.width+self.mountainSpaceF,1)
+
+
+class hills(land):
 	pass
 
-# class forest(land):
-# 	pass
+class forest(land):
+	pass
 
 if __name__ == '__main__':
 	width,height = 800,600
@@ -129,15 +194,15 @@ if __name__ == '__main__':
 	base = land(width,height,0,"clear")
 	testPlain = plain(width,height,0,"clear")
 	testRiver = river(width,height,0,"clear",2)
-	testHills = hills(width,height,0,"clear")
+	testmountains = mountains(width,height,0,"clear",1)
 	running = True
 	while running:
 		for event in pygame.event.get():
 			if(event.type == pygame.QUIT):
 				running = False
 		base.draw(screen)
+		testmountains.draw(screen)
 		testPlain.draw(screen)
 		testRiver.draw(screen)
 		pygame.display.flip()
-		# screen.blit(canvas,(0,0))
 	pygame.quit()
--- main.py
+++ main.py
@@ -1,39 +1,121 @@
 import pygame
 import scene, landscape
 
-#framework referenced from blog.lukasperaza.com and hermit 
-#code for taking commands taken from hermit
 
-def trainStart(width=1200,height=900):
+#framework referenced from blog.lukasperaza.com and hermit 
+#code for command console and taking commands based on hermit
+
+def trainStart(width=1600,height=1200):
 	def draw():
 		screen.fill((255,255,255))
 		seats.draw(canvas)
 		sceneryBase.draw(window)
+		mountains.draw(window)
 		plains.draw(window)
 		river.draw(window)
 		windowFrame.draw(window)
 		screen.blit(canvas,(0,0))
 		screen.blit(window,(0,0))
 
+	def drawConsole():
+		font =pygame.font.SysFont(pygame.font.get_default_font(),width//35)
+		if(showConsole):
+			screen.blit(console,[width*.01,height*.9])
+			#TODO show available commands
+			input = "".join(inputs)
+			text = font.render(":"+input,False,(150,150,150))
+			screen.blit(text,[width*.02,height*.91])
+
+	def infoGrab():
+		pass
+
+	def exeCommand(command):
+		failed = "Not Executed! :("
+		noSpace = command.split()
+		try:
+			commandType = noSpace[0]+" "+noSpace[1]
+			if(commandType not in commandlist):
+				return failed
+			return "hahahahaaaaa :'D"
+		except:
+			return failed
+
 	pygame.init()
 	screen = pygame.display.set_mode((width,height))
-	canvas= pygame.Surface((width,height))
-	window = pygame.Surface((width*2/3,height*2/3))
 	pygame.display.set_caption("choo-choo")
+
 	clock = pygame.time.Clock()
 	running = True
-	commandlist = ["set time","set month","set speed"]
-	color = (255,210,170)
+	showConsole = False
+	commandlist = {"set time","set month","set speed"}
+
+	color = (140,160,160)
+	inputs = ""
+	executed = False
+	speed = 0
+	start,end=False,False
+	startLoc,endLoc = "",""
+	
+	canvas= pygame.Surface((width,height))
+	window = pygame.Surface((width*2/3,height*2/3))
+	console = pygame.Surface((width/3,height/20))
+	console.fill([0,0,0])
+	console.set_alpha(50,pygame.RLEACCEL)
+
 	seats = scene.seats(width,height,color)
 	windowFrame = scene.frame(width,height,color)
 	sceneryBase = landscape.land(width*2/3,height*2/3, 0,"clear")
 	plains = landscape.plain(width*2/3,height*2/3,0,"clear")
 	river = landscape.river(width*2/3,height*2/3,0,"clear",2)
-	while running:
+	mountains = landscape.mountains(width*2/3,height*2/3,0,"clear",1)
+
+	while(running):
+		if(not start or not end): showConsole=True
 		for event in pygame.event.get():
 			if(event.type == pygame.QUIT):
 				running = False
+			elif(event.type == pygame.KEYDOWN):
+				key = pygame.key.name(event.key)
+				if(executed):
+					executed=False
+					inputs = ""
+				elif(key == "right" or key== "up"):
+					plains.changeSpeed(1)
+					river.changeSpeed(1)
+					mountains.changeSpeed(1)
+				elif(key == "left" or key == "down"):
+					plains.changeSpeed(-1)
+					river.changeSpeed(-1)
+					mountains.changeSpeed(-1)
+				elif(key == "/"):
+					showConsole = not showConsole
+					inputs = ""
+				elif(showConsole):
+					if(len(inputs)<=30):
+						if(key == "space"):
+							inputs +=" "
+						elif(key == "-"):
+							inputs+="-"
+						elif(len(key)==1):
+							inputs+=key
+						elif(key == "backspace" and len(inputs)>0):
+							inputs=inputs[:-1]
+					if(key == "return"):
+						if(not start):
+							startLoc = inputs
+							start=True
+							inputs = "Start location saved! :D"
+						elif(not end):
+							endLoc = inputs
+							end = True
+							inputs = "End location saved! :D"
+							infoGrab()
+						else:
+							inputs = str(exeCommand(str(inputs)))
+						executed = True
+						
 		draw()
+		drawConsole()
 		pygame.display.flip()
 		clock.tick()
 	pygame.quit()
